09a5ca67f34b30aa914b4347e6ec21b0b6f89e04
4Z1KD <4z1kd@iarc.org>
2025-04-04 12:58:55 +0300

    add thin border to the band bar legend

--- ui/src/components/FrequencyBar.jsx
+++ ui/src/components/FrequencyBar.jsx
@@ -409,7 +409,7 @@ export default function FrequencyBar({
                     <p
                         style={{ color: legend.color }}
                         key={`legend_${legend.name}`}
-                        className="text-[14px] inline px-2 bg-gray-100 rounded-full m-1"
+                        className="text-[14px] inline px-2 bg-gray-100 rounded-full m-1 border border-gray-300"
                     >
                         {legend.name.toUpperCase()}
                     </p>
