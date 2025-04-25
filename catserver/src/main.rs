5d66dff879534b531a8adff32facedaa6d18eda2
Omer Sarig <omersarig@hiveware.io>
2025-04-25 13:56:16 +0300

    Start implementing the websocket radio connection

--- catserver/src/main.rs
+++ catserver/src/main.rs
@@ -1,13 +1,20 @@
+use std::ops::ControlFlow;
+
 use axum::{
-    body::Body,
-    extract::{Request, State},
+    Router,
+    body::{Body, Bytes},
+    extract::{
+        Request, State, WebSocketUpgrade,
+        ws::{Message, WebSocket},
+    },
     http::{Response, StatusCode},
     response::IntoResponse,
     routing::any,
-    Router,
 };
 use axum_macros::debug_handler;
+use futures_util::{SinkExt, StreamExt};
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 
 const HOLY_CLUSTER_URL: &str = "https://holycluster.iarc.org";
 
@@ -15,7 +22,10 @@ const HOLY_CLUSTER_URL: &str = "https://holycluster.iarc.org";
 async fn main() {
     let client = Client::new();
 
-    let app = Router::new().fallback(any(proxy)).with_state(client);
+    let app = Router::new()
+        .route("/radio", any(ws_handler))
+        .fallback(any(proxy))
+        .with_state(client);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -44,4 +54,82 @@ async fn proxy(State(client): State<Client>, request: Request<Body>) -> Response
     response_builder
         .body(Body::from_stream(reqwest_response.bytes_stream()))
         .unwrap()
+}
+
+#[derive(Serialize, Deserialize)]
+struct StatusMessage {
+    status: String,
+    status_str: String,
+    freq: u64,
+    current_rig: u8,
+}
+
+async fn ws_handler(ws: WebSocketUpgrade) -> impl IntoResponse {
+    ws.on_upgrade(handle_socket)
+}
+
+async fn handle_socket(mut socket: WebSocket) {
+    if socket
+        .send(Message::Ping(Bytes::from_static(&[1, 2, 3])))
+        .await
+        .is_err()
+    {
+        return;
+    }
+
+    let (mut sender, mut receiver) = socket.split();
+
+    let message = StatusMessage {
+        status: "connected".to_string(),
+        status_str: "Status".to_string(),
+        freq: 14200000,
+        current_rig: 1,
+    };
+
+    let mut send_task = tokio::spawn(async move {
+        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+        sender
+            .send(Message::Text(
+                serde_json::to_string(&message).unwrap().into(),
+            ))
+            .await
+            .unwrap();
+
+        // let _ = sender
+        //     .send(Message::Close(Some(CloseFrame {
+        //         code: axum::extract::ws::close_code::NORMAL,
+        //         reason: Utf8Bytes::from_static("Goodbye"),
+        //     })))
+        //     .await;
+    });
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            if process_message(msg).is_break() {
+                break;
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => {
+            recv_task.abort();
+        },
+        _ = (&mut recv_task) => {
+            send_task.abort();
+        }
+    }
+}
+
+fn process_message(msg: Message) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(_text) => {}
+        Message::Binary(_data) => {}
+        Message::Close(_) => {
+            return ControlFlow::Break(());
+        }
+
+        _ => {}
+    }
+    ControlFlow::Continue(())
 }
