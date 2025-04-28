90577b07388295867b9ef538275714ffef510420
Omer Sarig <omersarig@hiveware.io>
2025-04-29 00:11:56 +0300

    Fix some invalid attributes

--- ui/src/assets/ToggleSwitchOn.jsx
+++ ui/src/assets/ToggleSwitchOn.jsx
@@ -10,8 +10,8 @@ const ToggleSwitchOn = () => {
             xmlns="http://www.w3.org/2000/svg"
         >
             <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M9 4C4.58172 4 1 7.58172 1 12C1 16.4183 4.58172 20 9 20H21C25.4183 20 29 16.4183 29 12C29 7.58172 25.4183 4 21 4H9ZM21 16C23.2091 16 25 14.2091 25 12C25 9.79086 23.2091 8 21 8C18.7909 8 17 9.79086 17 12C17 14.2091 18.7909 16 21 16Z"
                 fill="#4CAF50"
             />
