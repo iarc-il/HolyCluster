47b9787c53578afe7450ad6fd7660d9ed5a7e9e9
Omer Sarig <omersarig@hiveware.io>
2025-05-10 12:08:16 +0300

    testing

--- catserver/build.rs
+++ catserver/build.rs
@@ -1,11 +1,14 @@
-use {
-    std::{env, io},
-    winresource::WindowsResource,
-};
+use std::{env, io, path::Path};
+use winresource::WindowsResource;
 
 fn main() -> io::Result<()> {
     if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new().set_icon("wix/icon.ico").compile()?;
+        let icon_path = "wix/icon.ico";
+        if Path::new(icon_path).exists() {
+            WindowsResource::new().set_icon(icon_path).compile()?;
+        } else {
+            println!("cargo:warning=Icon not found at: {:?}", icon_path);
+        }
     }
     Ok(())
 }
