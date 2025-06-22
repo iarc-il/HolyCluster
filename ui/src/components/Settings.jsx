60cfa20ea486e79bf0e4acf69d235664747bb39e
Omer Sarig <omersarig@hiveware.io>
2025-06-23 01:13:18 +0300

    Basic implementation of import export mechanism

--- ui/src/components/Settings.jsx
+++ ui/src/components/Settings.jsx
@@ -7,6 +7,8 @@ import Modal from "@/components/Modal.jsx";
 import { useColors, themes_names } from "../hooks/useColors";
 import Toggle from "@/components/Toggle.jsx";
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { useFilters } from "@/hooks/useFilters";
+import ImportExport from "./settings/ImportExport";
 
 function SettingsIcon({ size }) {
     const { colors } = useColors();
@@ -102,152 +104,160 @@ function Settings({ settings, set_settings, set_map_controls, set_radius_in_km }
             }}
             on_cancel={() => reset_temp_settings()}
         >
-            <table
-                className="table-fixed w-[21rem] my-3 mx-2 border-separate border-spacing-y-2"
-                style={{ color: colors.theme.text }}
-            >
-                <tbody>
-                    <tr>
-                        <td>My callsign:</td>
-                        <td>
-                            <Input
-                                value={temp_settings.callsign}
-                                maxLength={11}
-                                className="uppercase"
-                                onChange={event => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        callsign: event.target.value.toUpperCase(),
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>My locator:</td>
-                        <td>
-                            <Input
-                                value={temp_settings.locator}
-                                className={is_locator_valid ? "" : "bg-red-200"}
-                                onChange={event => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        locator: event.target.value,
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Default map radius:</td>
-                        <td>
-                            <Input
-                                value={temp_settings.default_radius}
-                                className={is_default_radius_valid ? "" : "bg-red-200"}
-                                type="number"
-                                step="1000"
-                                min="1000"
-                                max="20000"
-                                onChange={event => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        default_radius: event.target.value,
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Theme:</td>
-                        <td>
-                            <Select
-                                value={temp_settings.theme}
-                                onChange={event => {
-                                    set_temp_settings(state => ({
-                                        ...state,
-                                        theme: event.target.value,
-                                    }));
-                                }}
-                            >
-                                {themes_names.map(name => {
-                                    return (
-                                        <option key={name} value={name}>
-                                            {name}
-                                        </option>
-                                    );
-                                })}
-                            </Select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Distance Units:&nbsp;&nbsp;</td>
-                        <td>
-                            <Select
-                                value={temp_settings.is_miles}
-                                onChange={event => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        is_miles: JSON.parse(event.target.value),
-                                    });
-                                }}
-                            >
-                                {[
-                                    { key: "km", value: false },
-                                    { key: "miles", value: true },
-                                ].map(unit => {
-                                    return (
-                                        <option key={unit.key} value={unit.value}>
-                                            {unit.key}
-                                        </option>
-                                    );
-                                })}
-                            </Select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Propagation:&nbsp;&nbsp;</td>
-                        <td>
-                            <Toggle
-                                value={temp_settings.propagation_displayed}
-                                on_click={() => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        propagation_displayed: !temp_settings.propagation_displayed,
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Show flags:&nbsp;&nbsp;</td>
-                        <td>
-                            <Toggle
-                                value={temp_settings.show_flags}
-                                on_click={() => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        show_flags: !temp_settings.show_flags,
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Show equator:&nbsp;&nbsp;</td>
-                        <td>
-                            <Toggle
-                                value={temp_settings.show_equator}
-                                on_click={() => {
-                                    set_temp_settings({
-                                        ...temp_settings,
-                                        show_equator: !temp_settings.show_equator,
-                                    });
-                                }}
-                            />
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="p-2">
+                <table
+                    className="table-fixed w-[21rem] my-3 border-separate border-spacing-y-2"
+                    style={{ color: colors.theme.text }}
+                >
+                    <tbody>
+                        <tr>
+                            <td>My callsign:</td>
+                            <td>
+                                <Input
+                                    value={temp_settings.callsign}
+                                    maxLength={11}
+                                    className="uppercase"
+                                    onChange={event => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            callsign: event.target.value.toUpperCase(),
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>My locator:</td>
+                            <td>
+                                <Input
+                                    value={temp_settings.locator}
+                                    className={is_locator_valid ? "" : "bg-red-200"}
+                                    onChange={event => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            locator: event.target.value,
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Default map radius:</td>
+                            <td>
+                                <Input
+                                    value={temp_settings.default_radius}
+                                    className={is_default_radius_valid ? "" : "bg-red-200"}
+                                    type="number"
+                                    step="1000"
+                                    min="1000"
+                                    max="20000"
+                                    onChange={event => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            default_radius: event.target.value,
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Theme:</td>
+                            <td>
+                                <Select
+                                    value={temp_settings.theme}
+                                    onChange={event => {
+                                        set_temp_settings(state => ({
+                                            ...state,
+                                            theme: event.target.value,
+                                        }));
+                                    }}
+                                >
+                                    {themes_names.map(name => {
+                                        return (
+                                            <option key={name} value={name}>
+                                                {name}
+                                            </option>
+                                        );
+                                    })}
+                                </Select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Distance Units:&nbsp;&nbsp;</td>
+                            <td>
+                                <Select
+                                    value={temp_settings.is_miles}
+                                    onChange={event => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            is_miles: JSON.parse(event.target.value),
+                                        });
+                                    }}
+                                >
+                                    {[
+                                        { key: "km", value: false },
+                                        { key: "miles", value: true },
+                                    ].map(unit => {
+                                        return (
+                                            <option key={unit.key} value={unit.value}>
+                                                {unit.key}
+                                            </option>
+                                        );
+                                    })}
+                                </Select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Propagation:&nbsp;&nbsp;</td>
+                            <td>
+                                <Toggle
+                                    value={temp_settings.propagation_displayed}
+                                    on_click={() => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            propagation_displayed:
+                                                !temp_settings.propagation_displayed,
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Show flags:&nbsp;&nbsp;</td>
+                            <td>
+                                <Toggle
+                                    value={temp_settings.show_flags}
+                                    on_click={() => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            show_flags: !temp_settings.show_flags,
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Show equator:&nbsp;&nbsp;</td>
+                            <td>
+                                <Toggle
+                                    value={temp_settings.show_equator}
+                                    on_click={() => {
+                                        set_temp_settings({
+                                            ...temp_settings,
+                                            show_equator: !temp_settings.show_equator,
+                                        });
+                                    }}
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <ImportExport
+                    settings={settings}
+                    set_settings={set_settings}
+                    set_temp_settings={set_temp_settings}
+                />
+            </div>
         </Modal>
     );
 }
---
+++ ui/src/components/settings/ImportExport.jsx
@@ -0,0 +1,129 @@
+import { useState } from "react";
+import Button from "@/components/Button.jsx";
+import Toggle from "@/components/Toggle.jsx";
+import { useColors } from "@/hooks/useColors";
+import { useFilters } from "@/hooks/useFilters";
+
+const EXPORTABLE_SETTINGS = {
+    settings: {
+        label: "General Settings",
+        description: "Includes locator, radius, theme, units, etc.",
+    },
+    filters: {
+        label: "Filters",
+        description: "Band, mode, and callsign filters",
+    },
+};
+
+function ImportExport({ settings, filters, set_settings, set_temp_settings, set_filters }) {
+    const { colors } = useColors();
+    const { filters, setFilters } = useFilters();
+
+    const [selected_settings, set_selected_settings] = useState(
+        Object.fromEntries(Object.keys(EXPORTABLE_SETTINGS).map(key => [key, true])),
+    );
+
+    function handle_export() {
+        const export_data = {};
+        Object.entries(selected_settings).forEach(([key, is_selected]) => {
+            if (is_selected) {
+                switch (key) {
+                    case "settings":
+                        export_data.settings = settings;
+                        break;
+                    case "filters":
+                        export_data.filters = filters;
+                        break;
+                }
+            }
+        });
+
+        const data_str = JSON.stringify(export_data, null, 2);
+        const data_blob = new Blob([data_str], { type: "application/json" });
+        const url = URL.createObjectURL(data_blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "holycluster-settings.json";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        URL.revokeObjectURL(url);
+    }
+
+    function handle_import(event) {
+        const file = event.target.files[0];
+        if (!file) return;
+
+        const reader = new FileReader();
+        reader.onload = event => {
+            try {
+                const imported_data = JSON.parse(event.target.result);
+
+                Object.entries(selected_settings).forEach(([key, is_selected]) => {
+                    if (is_selected && imported_data[key]) {
+                        switch (key) {
+                            case "settings":
+                                set_settings(imported_data.settings);
+                                set_temp_settings(imported_data.settings);
+                                break;
+                            case "filters":
+                                set_filters(imported_data.filters);
+                                break;
+                        }
+                    }
+                });
+            } catch (error) {
+                console.error("Error importing settings:", error);
+            }
+        };
+        reader.readAsText(file);
+        event.target.value = null;
+    }
+
+    return (
+        <div className="border-t mt-4 pt-4" style={{ color: colors.theme.text }}>
+            <h3 className="text-xl mb-4">Import/Export Settings</h3>
+            <div className="space-y-2 mb-4">
+                {Object.entries(EXPORTABLE_SETTINGS).map(([key, { label, description }]) => (
+                    <div key={key} className="flex items-center justify-between">
+                        <div>
+                            <span className="font-medium">{label}</span>
+                            <p className="text-sm opacity-75">{description}</p>
+                        </div>
+                        <Toggle
+                            value={selected_settings[key]}
+                            on_click={() =>
+                                set_selected_settings(prev => ({
+                                    ...prev,
+                                    [key]: !prev[key],
+                                }))
+                            }
+                        />
+                    </div>
+                ))}
+            </div>
+            <div className="flex justify-between mt-4">
+                <div>
+                    <input
+                        type="file"
+                        accept=".json"
+                        onChange={handle_import}
+                        className="hidden"
+                        id="settings-import"
+                    />
+                    <Button
+                        color="blue"
+                        on_click={() => document.getElementById("settings-import").click()}
+                    >
+                        Import from File
+                    </Button>
+                </div>
+                <Button color="green" on_click={handle_export}>
+                    Export to File
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default ImportExport;
