9fc81994b2631203f007079470e576984b1233ff
beiser <roy.beiser@gmail.com>
2025-08-10 23:52:48 +0300

    Added debug to open_log_file

--- backend/collectors/src/misc.py
+++ backend/collectors/src/misc.py
@@ -9,14 +9,14 @@ def string_to_boolean(value: str) -> bool:
 
 
 
-def open_log_file(log_filename_prefix):
+def open_log_file(log_filename_prefix: str, debug: bool = False):
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_filename = f"{log_filename_prefix}.{timestamp}.log"
     logger.info(f"Opening log file at: {log_filename}")
     try:
-        # Add a single sink for all log levels.
         logger.add(log_filename, rotation="10 MB")
         logger.info(f"log file: {log_filename}")
+        return log_filename 
 
     except Exception as ex:
         template = "**** ERROR OPENING LOG FILE **** An exception of type {0} occured. Arguments: {1!r}"
