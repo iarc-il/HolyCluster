de321355958c9db573e831471452a448d502b839
beiser <roy.beiser@gmail.com>
2025-10-24 21:43:19 +0300

    Remove SSL from API settings

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -13,9 +13,5 @@ DATABASE = "holy_cluster"
 GENERAL_DB_URL = f"postgresql+psycopg2://{PSQL_USERNAME}:{PSQL_PASSWORD}@{HOST}:{PORT}"
 DB_URL = f"{GENERAL_DB_URL}/{DATABASE}"
 
-SSL_KEYFILE = env.str("SSL_KEYFILE", None)
-SSL_CERTFILE = env.str("SSL_CERTFILE", None)
-SSL_AVAILABLE = SSL_KEYFILE is not None and SSL_CERTFILE is not None
-
 UI_DIR = env.path("UI_DIR")
 CATSERVER_MSI_DIR = env.path("CATSERVER_MSI_DIR")
