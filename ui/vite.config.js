91ae3f6087747df36523f084953a4efad97994b0
Omer Sarig <omersarig@hiveware.io>
2025-10-25 20:51:39 +0300

    Split the UI to manual chunks

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,12 +1,31 @@
 import path from "path";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-    plugins: [react(), splitVendorChunkPlugin()],
+    plugins: [react(), visualizer()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: function(id) {
+                    if (id.includes("node_modules")) {
+                        return "vendor";
+                    }
+                    if (id.includes("dxcc_map.json")) {
+                        return "dxcc";
+                    }
+                    if (id.includes("flags.json")) {
+                        return "flags";
+                    }
+                    return "index";
+                },
+            }
+        }
+    }
 });
