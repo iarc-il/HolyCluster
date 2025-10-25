f88465012475b9828c755bcdeb24e98f7cdfd1cf
beiser <roy.beiser@gmail.com>
2025-10-25 23:05:21 +0300

    Fix run_collector.sh script

--- backend/src/run_collector.sh
+++ backend/src/run_collector.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 cd /opt/HolyCluster/
 source /opt/HolyCluster/.venv/bin/activate
-/opt/HolyCluster/.venv/bin/python3 /opt/HolyCluster/src/run_collector.py 
+/opt/HolyCluster/.venv/bin/python3 /opt/HolyCluster/backend/src/run_collector.py 
