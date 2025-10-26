19850e396bd918167b3910437929b51866b3c005
beiser <roy.beiser@gmail.com>
2025-10-26 19:16:13 +0200

    Initial commit - enrich_spots.py

---
+++ backend/collectors/src/enricher/enrich_spots.py
@@ -0,0 +1,15 @@
+import re
+import csv
+from typing import List
+from pathlib import Path
+from loguru import logger
+
+
+def load_frequency_table():
+    pass
+
+
+def find_band_and_mode(frequency:str, comment:str, debug: bool=False)->List:
+    pass
+
+
