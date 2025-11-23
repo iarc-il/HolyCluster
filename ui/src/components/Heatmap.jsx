fb53be3b3268097d0a14a104f06007632fa9ebaf
Omer Sarig <omersarig@hiveware.io>
2025-11-23 23:37:41 +0200

    Add explenation text under the heatmap

--- ui/src/components/Heatmap.jsx
+++ ui/src/components/Heatmap.jsx
@@ -213,6 +213,11 @@ function Heatmap() {
                     }}
                 />
             </div>
+
+            <div className="text-xs text-center p-4" style={{ color: colors.theme.text }}>
+                The heatmap is calculated from the reported spots from/to the selected continents in
+                the last hour.
+            </div>
         </div>
     );
 }
