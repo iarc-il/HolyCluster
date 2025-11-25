7f1c2db62822b043f4f7d0ae93baf5945e71b418
Omer Sarig <omersarig@hiveware.io>
2025-11-25 19:30:05 +0200

    Remove hiding exceptions

--- backend/collectors/src/db/add_enriched_telnet_spots_to_postgres.py
+++ backend/collectors/src/db/add_enriched_telnet_spots_to_postgres.py
@@ -63,28 +63,19 @@ async def convert_spot_to_record(spot: dict, debug: bool = False):
 async def add_spot_to_postgres(spot: dict, debug: bool = False):
     if debug:
         logger.debug(f"Attempting to add spot records to the database.")
-    try:
-        record = await convert_spot_to_record(spot=spot, debug=debug)
-        engine = create_async_engine(POSTGRES_DB_URL, echo=debug)
-        async with AsyncSession(engine) as session:
-            session.add(record)
 
-            try:
-                session.add(record)
-                await session.commit()
-                await session.refresh(record)
-                logger.debug(f"{record.id=}")
-                logger.info(f"Successfully pushed spot record (duplicates ignored).")
-            except Exception as e:
-                await session.rollback()
-                logger.error(f"Failed to push spot records to database: {e}")
-        await engine.dispose()
+    record = await convert_spot_to_record(spot=spot, debug=debug)
+    import ipdb;ipdb.set_trace()
+    engine = create_async_engine(POSTGRES_DB_URL, echo=debug)
 
-    except Exception as ex:
-        message = f"**** ERROR postress_spots_consumer **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
-        logger.error(message)
-    #
-    return
+    async with AsyncSession(engine) as session:
+        session.add(record)
+        await session.commit()
+        await session.refresh(record)
+        logger.debug(f"{record.id=}")
+        logger.info(f"Successfully pushed spot record (duplicates ignored).")
+
+    await engine.dispose()
 
 
 async def postgres_spots_consumer(debug: bool = False):
@@ -112,29 +103,24 @@ async def postgres_spots_consumer(debug: bool = False):
     last_id = ">"
 
     while True:
-        try:
-            # Block until a message arrives
-            resp = valkey_client.xreadgroup(
-                CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: last_id}, count=10, block=60000
-            )
-            if not resp:
-                continue
+        # Block until a message arrives
+        resp = valkey_client.xreadgroup(
+            CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: last_id}, count=10, block=60000
+        )
+        if not resp:
+            continue
 
-            for stream_name, messages in resp:
-                for msg_id, spot in messages:
-                    if debug:
-                        logger.debug(f"{msg_id=}")
-                        logger.debug(f"spot={json.dumps(spot, indent=4)}")
-                    valkey_client.xack(STREAM_NAME, CONSUMER_GROUP, msg_id)
-                    valkey_client.xtrim(STREAM_NAME, minid=msg_id, approximate=False)
+        for stream_name, messages in resp:
+            for msg_id, spot in messages:
+                if debug:
+                    logger.debug(f"{msg_id=}")
+                    logger.debug(f"spot={json.dumps(spot, indent=4)}")
+                valkey_client.xack(STREAM_NAME, CONSUMER_GROUP, msg_id)
+                valkey_client.xtrim(STREAM_NAME, minid=msg_id, approximate=False)
 
-                    # add spot to postgres
-                    logger.info(f"{spot=}")
-                    await add_spot_to_postgres(spot=spot, debug=debug)
-
-        except Exception as ex:
-            message = f"**** ERROR postress_spots_consumer **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
-            logger.error(message)
+                # add spot to postgres
+                logger.info(f"{spot=}")
+                await add_spot_to_postgres(spot=spot, debug=debug)
 
 
 if __name__ == "__main__":
