da306ea36a7e7d7c1728063bace7c23df8ccc60d
Omer Sarig <omersarig@hiveware.io>
2025-11-25 19:23:46 +0200

    Execute format

--- backend/collectors/src/db/add_enriched_telnet_spots_to_postgres.py
+++ backend/collectors/src/db/add_enriched_telnet_spots_to_postgres.py
@@ -1,14 +1,9 @@
-from datetime import datetime, timezone
-import time
-import argparse
+from datetime import datetime
 import os
-import re
 import json
 import asyncio
 from loguru import logger
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlmodel import SQLModel, select
-from sqlalchemy.dialects.postgresql import insert
 import redis
 
 from misc import open_log_file, in_docker
@@ -23,48 +18,46 @@ from settings import (
     VALKEY_DB,
     POSTGRES_DB_URL,
 )
+
 if not in_docker():
-    VALKEY_HOST = VALKEY_HOST_LOCAL 
+    VALKEY_HOST = VALKEY_HOST_LOCAL
     VALKEY_PORT = VALKEY_PORT_LOCAL
 
-from postgres_classes import HolySpot2, SpotWithIssue2
+from postgres_classes import HolySpot2
 
 global valkey_client
 valkey_client = get_valkey_client(host=VALKEY_HOST, port=VALKEY_PORT, db=VALKEY_DB)
 
+
 async def convert_spot_to_record(spot: dict, debug: bool = False):
-    dt = datetime.strptime(spot['date_time'], "%Y-%m-%d %H:%M:%S%z")
+    dt = datetime.strptime(spot["date_time"], "%Y-%m-%d %H:%M:%S%z")
     dt_naive = dt.replace(tzinfo=None)
     record = HolySpot2(
-        # date = datetime.strptime(spot['Date'], '%d/%m/%y').date()
-        cluster = spot['cluster'], 
-        # time = datetime.strptime(spot['date_time'], "%Y-%m-%d %H:%M:%S%z").strftime("%H:%M"),
-        time = dt.time(),
-        # date_time = datetime.strptime(spot['date_time'], "%Y-%m-%d %H:%M:%S%z"),
-        date_time = dt_naive,
-        frequency = spot['frequency'], 
-        band = spot['band'], 
-        mode = spot['mode'], 
-        mode_selection = spot['mode_selection'], 
-        spotter_callsign = spot['spotter_callsign'], 
-        spotter_locator = spot['spotter_locator'], 
-        spotter_locator_source = spot['spotter_locator_source'], 
-        spotter_lat = spot['spotter_lat'], 
-        spotter_lon = spot['spotter_lon'], 
-        spotter_country = spot['spotter_country'], 
-        spotter_continent = spot['spotter_continent'], 
-        dx_callsign = spot['dx_callsign'], 
-        dx_locator = spot['dx_locator'], 
-        dx_locator_source = spot['dx_locator_source'], 
-        dx_lat = spot['dx_lat'], 
-        dx_lon = spot['dx_lon'], 
-        dx_country = spot['dx_country'], 
-        dx_continent = spot['dx_continent'], 
-        comment = spot['comment'] 
+        cluster=spot["cluster"],
+        time=dt.time(),
+        date_time=dt_naive,
+        frequency=spot["frequency"],
+        band=spot["band"],
+        mode=spot["mode"],
+        mode_selection=spot["mode_selection"],
+        spotter_callsign=spot["spotter_callsign"],
+        spotter_locator=spot["spotter_locator"],
+        spotter_locator_source=spot["spotter_locator_source"],
+        spotter_lat=spot["spotter_lat"],
+        spotter_lon=spot["spotter_lon"],
+        spotter_country=spot["spotter_country"],
+        spotter_continent=spot["spotter_continent"],
+        dx_callsign=spot["dx_callsign"],
+        dx_locator=spot["dx_locator"],
+        dx_locator_source=spot["dx_locator_source"],
+        dx_lat=spot["dx_lat"],
+        dx_lon=spot["dx_lon"],
+        dx_country=spot["dx_country"],
+        dx_continent=spot["dx_continent"],
+        comment=spot["comment"],
     )
 
     return record
-
 
 
 async def add_spot_to_postgres(spot: dict, debug: bool = False):
@@ -75,25 +68,12 @@ async def add_spot_to_postgres(spot: dict, debug: bool = False):
         engine = create_async_engine(POSTGRES_DB_URL, echo=debug)
         async with AsyncSession(engine) as session:
             session.add(record)
-            # records_data = []
-            # record_dict = {k: v for k, v in record.__dict__.items() if not k.startswith('_')}
-            # records_data.append(record_dict)
-            #
-            # # Construct the insert statement with ON CONFLICT DO NOTHING
-            # insert_stmt = insert(HolySpot2).values(records_data)
-            # on_conflict_stmt = insert_stmt.on_conflict_do_nothing(
-            #     index_elements=["time", "spotter_callsign",  "frequency", "dx_callsign"] # Specify the unique index columns
-            # )
 
             try:
                 session.add(record)
                 await session.commit()
                 await session.refresh(record)
-                logger.debug(f"{record.id=}")                # on_conflict_stmt = insert_stmt.on_conflict_do_nothing(
-                #     index_elements=["time", "spotter_callsign",  "frequency", "dx_callsign"] # Specify the unique index columns
-                # )
-                # await session.execute(on_conflict_stmt)
-                # await session.commit()
+                logger.debug(f"{record.id=}")
                 logger.info(f"Successfully pushed spot record (duplicates ignored).")
             except Exception as e:
                 await session.rollback()
@@ -107,13 +87,12 @@ async def add_spot_to_postgres(spot: dict, debug: bool = False):
     return
 
 
-
 async def postgres_spots_consumer(debug: bool = False):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     log_filename = f"add_enrich_telnet_spots"
-    log_dir = os.path.join(script_dir, '..', '..', 'logs')
+    log_dir = os.path.join(script_dir, "..", "..", "logs")
     os.makedirs(log_dir, exist_ok=True)
-    telnet_log_dir = os.path.join(log_dir, 'db')
+    telnet_log_dir = os.path.join(log_dir, "db")
     os.makedirs(telnet_log_dir, exist_ok=True)
     log_path = os.path.join(telnet_log_dir, log_filename)
     open_log_file(log_filename_prefix=log_path, debug=debug)
@@ -122,19 +101,22 @@ async def postgres_spots_consumer(debug: bool = False):
     STREAM_NAME = "stream-postres"
     CONSUMER_GROUP = "postres-group"
     CONSUMER_NAME = "consumer_1"
-# Create consumer group (only first time)
+
+    # Create consumer group (only first time)
     try:
-        valkey_client.xgroup_create(STREAM_NAME, CONSUMER_GROUP, id='0', mkstream=True)
+        valkey_client.xgroup_create(STREAM_NAME, CONSUMER_GROUP, id="0", mkstream=True)
     except redis.exceptions.ResponseError:
         # Group already exists
         pass
 
-    last_id = '>'
+    last_id = ">"
 
     while True:
         try:
             # Block until a message arrives
-            resp = valkey_client.xreadgroup(CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: last_id}, count=10, block=60000)
+            resp = valkey_client.xreadgroup(
+                CONSUMER_GROUP, CONSUMER_NAME, {STREAM_NAME: last_id}, count=10, block=60000
+            )
             if not resp:
                 continue
 
@@ -149,12 +131,11 @@ async def postgres_spots_consumer(debug: bool = False):
                     # add spot to postgres
                     logger.info(f"{spot=}")
                     await add_spot_to_postgres(spot=spot, debug=debug)
-                    
 
         except Exception as ex:
             message = f"**** ERROR postress_spots_consumer **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
             logger.error(message)
 
+
 if __name__ == "__main__":
     asyncio.run(postgres_spots_consumer(debug=DEBUG))
-
