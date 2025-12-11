8da1bda12403c56c942c5848a1040e8d1deb65ab
Omer Sarig <omersarig@hiveware.io>
2025-12-11 18:57:48 +0200

    Fix some issues in the settings file

--- backend/api/src/api/settings.py
+++ backend/api/src/api/settings.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env()
+env.read_env(".env")
 
 IN_DOCKER = Path("/.dockerenv").exists()
 
@@ -14,18 +14,18 @@ SPOTS_LOG_PATH = env.str("SPOTS_LOG_PATH")
 if IN_DOCKER:
     POSTGRES_HOST = env.str("POSTGRES_HOST")
     POSTGRES_PORT = env.str("POSTGRES_PORT")
+    VALKEY_HOST = env.str("VALKEY_HOST")
+    VALKEY_PORT = env.int("VALKEY_PORT")
 else:
     POSTGRES_HOST = env.str("POSTGRES_HOST_LOCAL")
     POSTGRES_PORT = env.str("POSTGRES_PORT_LOCAL")
+    VALKEY_HOST = env.str("VALKEY_HOST_LOCAL")
+    VALKEY_PORT = env.int("VALKEY_PORT_LOCAL")
 
-VALKEY_HOST = env.str("VALKEY_HOST")
-VALKEY_PORT = env.int("VALKEY_PORT")
 
 DATABASE = env.str("POSTGRES_DB_NAME", default="holy_cluster")
 GENERAL_DB_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}"
 DB_URL = f"{GENERAL_DB_URL}/{DATABASE}"
 
-# TODO: The defaults are copied from ../config/paths.env,
-# We should find a way to use it as a fallback
-UI_DIST_PATH = env.path("UI_DIST_PATH", default="/opt/ui-dist")
-CATSERVER_MSI_DIR = env.path("CATSERVER_MSI_PATH", default="/opt/msi")
+UI_DIST_PATH = env.path("UI_DIST_PATH")
+CATSERVER_MSI_DIR = env.path("CATSERVER_MSI_DIR")
