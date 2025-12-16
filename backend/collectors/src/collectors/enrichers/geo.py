cb2c41295b21d519677053ce146b31ae86578914
Omer Sarig <omersarig@hiveware.io>
2025-12-16 23:49:16 +0200

    Execute format

--- backend/collectors/src/collectors/enrichers/geo.py
+++ backend/collectors/src/collectors/enrichers/geo.py
@@ -18,9 +18,8 @@ try:
     if not socket.gethostbyname(VALKEY_HOST):
         pass
 except socket.gaierror as e:
-    VALKEY_HOST="127.0.0.1"
+    VALKEY_HOST = "127.0.0.1"
 valkey_client = get_valkey_client(host=VALKEY_HOST, port=VALKEY_PORT, db=VALKEY_DB)
-
 
 
 async def check_geo_cache(callsign: str, debug: bool = False):
@@ -31,7 +30,9 @@ async def check_geo_cache(callsign: str, debug: bool = False):
             logger.debug(f"{geo_cache_details=}")
 
     except Exception as ex:
-        message = f"**** ERROR check_geo_cache **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
+        message = (
+            f"**** ERROR check_geo_cache **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
+        )
         logger.error(message)
 
     finally:
@@ -41,16 +42,18 @@ async def check_geo_cache(callsign: str, debug: bool = False):
 async def add_geo_cache(callsign: str, geo_details_str: str, debug: bool = False):
     if debug:
         logger.debug(f"Adding to geo_cache: {geo_details_str=}")
-#
+    #
     try:
         valkey_client.set(callsign, geo_details_str, ex=VALKEY_GEO_EXPIRATION)
 
     except Exception as ex:
-        message = f"**** ERROR check_geo_cache **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
+        message = (
+            f"**** ERROR check_geo_cache **** An exception of type {type(ex).__name__} occured. Arguments: {ex.args}"
+        )
         logger.error(message)
 
     finally:
-        return 
+        return
 
 
 async def get_geo_details(qrz_session_key: str, callsign: str, debug: bool = False):
@@ -65,58 +68,54 @@ async def get_geo_details(qrz_session_key: str, callsign: str, debug: bool = Fal
     geo_cache_details = await check_geo_cache(callsign=callsign, debug=debug)
 
     if geo_cache_details:
-        geo_cache = 1 
+        geo_cache = 1
         geo = json.loads(geo_cache_details)
-        locator = geo['locator']
-        locator_source = geo['locator_source']
-        lat = geo['lat']
-        lon = geo['lon']
-        country = geo['country']
-        continent = geo['continent']
+        locator = geo["locator"]
+        locator_source = geo["locator_source"]
+        lat = geo["lat"]
+        lon = geo["lon"]
+        country = geo["country"]
+        continent = geo["continent"]
 
     else:
         geo_cache = 0
         # Get locator from qrz
         qrz_locator_dict = await get_locator_from_qrz(
-            qrz_session_key=qrz_session_key, 
-            callsign=callsign,
-            delay=0, 
-            debug=debug
+            qrz_session_key=qrz_session_key, callsign=callsign, delay=0, debug=debug
         )
         if qrz_locator_dict is None:
             logger.error(f"get_locator_from_qrz returned None for {callsign}, falling back to prefix list")
             qrz_locator_dict = {"locator": None, "error": "Function returned None"}
 
-        locator = qrz_locator_dict.get('locator')
+        locator = qrz_locator_dict.get("locator")
         if locator:
             locator_source = "qrz"
         else:
             # Get locator from prefixes list (CSV file)
-            if 'error' in qrz_locator_dict:
+            if "error" in qrz_locator_dict:
                 if debug:
                     logger.debug(f"{qrz_locator_dict['error']}")
             locator = resolve_locator_from_list(callsign=callsign, debug=debug)
             if locator:
                 locator_source = "prefixes list"
-        
+
         if locator:
             # Get country & continent from prefixed list (CSV file)
             country, continent = resolve_country_and_continent_from_list(callsign=callsign, debug=debug)
             # Calculate coordinates
             lat, lon = locator_to_coordinates(locator)
-        
+
             # Add geo details to cache
             geo_details_dict = {
-                'locator_source': locator_source,
-                'locator' : locator,
-                'lat' : lat,
-                'lon' : lon,
-                'country' : country,
-                'continent' : continent
+                "locator_source": locator_source,
+                "locator": locator,
+                "lat": lat,
+                "lon": lon,
+                "country": country,
+                "continent": continent,
             }
             geo_details_str = json.dumps(geo_details_dict)
             await add_geo_cache(callsign=callsign, geo_details_str=geo_details_str, debug=debug)
-
 
     if debug:
         logger.debug(f"{callsign=}")
