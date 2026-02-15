913a9825d5e529a77eeacbdd9cbbd2e4e6452b41
Omer Sarig <omersarig@hiveware.io>
2026-02-15 23:44:35 +0200

    Fix too big flag column in the spots table

--- ui/src/components/SpotsTable.jsx
+++ ui/src/components/SpotsTable.jsx
@@ -537,7 +537,7 @@ function SpotsTable({ table_sort, set_table_sort, set_cat_to_spot }) {
                                 />
                                 <HeaderCell
                                     title=""
-                                    field=""
+                                    field="flag"
                                     cell_classes={cell_classes}
                                     table_sort={table_sort}
                                     set_table_sort={set_table_sort}
