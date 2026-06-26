9e093172fda2fd7c42e419ff9de1ba13a0d44b24
Omer Sarig <omer@sarig.co.il>
2026-06-26 20:17:34 +0300

    Add missing file

---
+++ ui/src/data/hunter_sections.js
@@ -0,0 +1,9 @@
+export const HUNTER_SECTION_KEYS = ["dxcc", "cq_zone", "itu_zone", "us_state", "ca_province"];
+
+export const HUNTER_SECTION_LABELS = {
+    dxcc: "DXCC",
+    cq_zone: "CQ",
+    itu_zone: "ITU",
+    us_state: "US",
+    ca_province: "Canada",
+};
