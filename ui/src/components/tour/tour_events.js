1151ee1bcb91ea83c90c54d263aea240c1932875
Omer Sarig <omer@sarig.co.il>
2026-07-02 17:59:27 +0300

    Add missing file

---
+++ ui/src/components/tour/tour_events.js
@@ -0,0 +1,3 @@
+export const TOUR_CLOSE_MAP_CONTROLS_EVENT = "holycluster:tour-close-map-controls";
+export const TOUR_CLOSE_MODAL_EVENT = "holycluster:tour-close-modal";
+export const TOUR_FILTER_OPTIONS_EVENT = "holycluster:tour-filter-options";
