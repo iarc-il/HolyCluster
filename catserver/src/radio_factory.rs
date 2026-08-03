2d32c1cf8435eda1b5e458bf242e2e0e2ea92faf
Omer Sarig <omer@sarig.co.il>
2026-08-03 22:54:51 +0300

    Configure rigctld endpoints

--- catserver/src/radio_factory.rs
+++ catserver/src/radio_factory.rs
@@ -11,11 +11,19 @@ use crate::omnirig::OmnirigRadio;
 #[cfg(not(windows))]
 use crate::rigctld::RigctldRadio;
 
-pub(crate) fn factory(config: RadioConfig, selected: ActiveRadioBackend) -> RadioFactory {
-    std::sync::Arc::new(move || build(&config, &selected))
+pub(crate) fn factory(
+    config: RadioConfig,
+    selected: ActiveRadioBackend,
+    rigctld_endpoint: (String, u16),
+) -> RadioFactory {
+    std::sync::Arc::new(move || build(&config, &selected, &rigctld_endpoint))
 }
 
-fn build(config: &RadioConfig, selected: &ActiveRadioBackend) -> Box<dyn Radio> {
+fn build(
+    config: &RadioConfig,
+    selected: &ActiveRadioBackend,
+    rigctld_endpoint: &(String, u16),
+) -> Box<dyn Radio> {
     match selected {
         ActiveRadioBackend::Dummy => Box::new(DummyRadio::new()),
         ActiveRadioBackend::Configured(RadioBackendKind::Hamlib) => config
@@ -28,7 +36,10 @@ fn build(config: &RadioConfig, selected: &ActiveRadioBackend) -> Box<dyn Radio> 
         ActiveRadioBackend::Configured(RadioBackendKind::Omnirig) => Box::new(OmnirigRadio::new()),
         #[cfg(not(windows))]
         ActiveRadioBackend::Configured(RadioBackendKind::Rigctld) => {
-            Box::new(RigctldRadio::new("localhost".into(), 4532))
+            Box::new(RigctldRadio::new(
+                rigctld_endpoint.0.clone(),
+                rigctld_endpoint.1,
+            ))
         }
         ActiveRadioBackend::Configured(backend) => Box::new(UnavailableRadio::new(match backend {
             RadioBackendKind::Omnirig => "omnirig",
