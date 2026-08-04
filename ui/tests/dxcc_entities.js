805c8257321661ce3320bbdb42c0b62e975da9e2
Omer Sarig <omer@sarig.co.il>
2026-08-04 17:32:11 +0300

    Use DXCC fixtures in tests

--- ui/tests/dxcc_entities.js
+++ ui/tests/dxcc_entities.js
@@ -1,9 +1,17 @@
 import { describe, expect, it, vi } from "vitest";
 
 vi.mock("virtual:cty-dxcc-entities", () => ({
-    default: [],
-    dxcc_entities_by_code: {},
-    dxcc_code_entities: {},
+    default: ["United States", "Fed. Rep. of Germany", "Canada"],
+    dxcc_entities_by_code: {
+        1: { code: 1, raw_cty_name: "Canada", continent: "NA" },
+        230: { code: 230, raw_cty_name: "Fed. Rep. of Germany", continent: "EU" },
+        291: { code: 291, raw_cty_name: "United States", continent: "NA" },
+    },
+    dxcc_code_entities: {
+        1: "Canada",
+        230: "Fed. Rep. of Germany",
+        291: "United States",
+    },
 }));
 
 import { build_dxcc_entity_by_code, get_dxcc_label } from "@/data/dxcc_entities.js";
