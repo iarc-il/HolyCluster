24d3a1adb53695828859c056aac85f155594f466
Omer Sarig <omer@sarig.co.il>
2026-09-18 22:24:11 +0300

    Make Windows resources deterministic

--- catserver/build.rs
+++ catserver/build.rs
@@ -6,9 +6,6 @@ use {
 fn main() -> io::Result<()> {
     println!("cargo:rerun-if-changed=sentry_dsn.txt");
     println!("cargo:rerun-if-env-changed=CATSERVER_SENTRY_ENVIRONMENT");
-    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
-        WindowsResource::new().set_icon("wix/icon.ico").compile()?;
-    }
 
     let output = Command::new("git")
         .args(["describe", "--match", "catserver-v*"])
@@ -26,6 +23,15 @@ fn main() -> io::Result<()> {
         panic!("Invalid catserver release: {version}");
     }
     println!("cargo:rustc-env=VERSION={version}");
+
+    if env::var_os("CARGO_CFG_WINDOWS").is_some() {
+        println!("cargo:rerun-if-changed=wix/icon.ico");
+        let resource = write_windows_resource(version)?;
+        WindowsResource::new()
+            .set_resource_file(resource.to_str().unwrap())
+            .compile()?;
+    }
+
     let sentry_environment =
         env::var("CATSERVER_SENTRY_ENVIRONMENT").unwrap_or_else(|_| "dev".into());
     if !matches!(sentry_environment.as_str(), "dev" | "prod") {
@@ -44,4 +50,60 @@ fn main() -> io::Result<()> {
     println!("cargo:rustc-env=CATSERVER_SENTRY_DSN={sentry_dsn}");
 
     Ok(())
+}
+
+fn write_windows_resource(version: &str) -> io::Result<PathBuf> {
+    let release = version
+        .strip_prefix("catserver-v")
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "invalid release version"))?;
+    let mut release_parts = release.split('-');
+    let base = release_parts.next().unwrap();
+    let commit_count = release_parts
+        .next()
+        .map(str::parse::<u16>)
+        .transpose()
+        .map_err(|error| io::Error::new(io::ErrorKind::InvalidData, error))?
+        .unwrap_or(0);
+    let numbers = base
+        .split('.')
+        .map(str::parse::<u16>)
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|error| io::Error::new(io::ErrorKind::InvalidData, error))?;
+    let [major, minor, patch] = numbers.as_slice() else {
+        return Err(io::Error::new(
+            io::ErrorKind::InvalidData,
+            "release version must contain three numbers",
+        ));
+    };
+    let contents = format!(
+        r#"#pragma code_page(65001)
+1 VERSIONINFO
+FILETYPE 0x1
+FILESUBTYPE 0x0
+FILEFLAGSMASK 0x3f
+FILEFLAGS 0x0
+FILEOS 0x40004
+FILEVERSION {major}, {minor}, {patch}, {commit_count}
+PRODUCTVERSION {major}, {minor}, {patch}, {commit_count}
+{{
+BLOCK "StringFileInfo"
+{{
+BLOCK "000004b0"
+{{
+VALUE "FileDescription", "catserver"
+VALUE "FileVersion", "{release}"
+VALUE "ProductName", "catserver"
+VALUE "ProductVersion", "{release}"
+}}
+}}
+BLOCK "VarFileInfo" {{
+VALUE "Translation", 0x0, 0x04b0
+}}
+}}
+1 ICON "wix/icon.ico"
+"#
+    );
+    let path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("resource.rc");
+    fs::write(&path, contents)?;
+    Ok(path)
 }
