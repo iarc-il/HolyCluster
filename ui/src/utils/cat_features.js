6d4eb72691e4a5e729dcf7e87b46b3c458bfa89a
Omer Sarig <omer@sarig.co.il>
2026-09-22 20:39:48 +0300

    Enable updater after Catserver 1.2.0

--- ui/src/utils/cat_features.js
+++ ui/src/utils/cat_features.js
@@ -1,6 +1,6 @@
 import { compare_version } from "@/utils.js";
 
-export const NATIVE_UPDATER_MIN_VERSION = [1, 3, 0, 0];
+export const NATIVE_UPDATER_MIN_VERSION = [1, 2, 0, 1];
 export const ROTATOR_MIN_VERSION = [1, 2, 0, 1];
 export const RTTY_TUNING_MIN_VERSION = [1, 3, 0, 0];
 
